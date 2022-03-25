4fab3a42e20c8665f8749e824b529c635ec767f0
Nikusha gagua <ngagua@gmail.com>
2022-03-25 16:26:28 +0400

    final commit

--- src/app/highlight.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { HighlightDirective } from './directives/highlight.directive';
-
-describe('HighlightDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HighlightDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/pipes/addBlock.pipe.ts
+++ src/app/pipes/addBlock.pipe.ts
@@ -4,7 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'addBlock',
 })
 export class AddBlockPipe implements PipeTransform {
-  transform(value: Array<number>, number = 7): any {
+  transform(value: Array<number>, number: number): Array<number> {
     for (let i = 0; i < number; i++) {
       value.push(i);
     }
